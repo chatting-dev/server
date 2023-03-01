c34d4100708c3eda0b3fe594d571c82d9e78ca9c
Jeongyong Park <1239824@naver.com>
2023-03-01 15:03:41 +0900

    [feat] chat_join 엔티티 추가

---
+++ chatting/src/main/java/com/chatting/domain/chatjoin/ChatJoin.java
@@ -0,0 +1,52 @@
+package com.chatting.domain.chatjoin;
+
+import java.util.Objects;
+
+import com.chatting.domain.AuditingFields;
+import com.chatting.domain.chatroom.ChatRoom;
+import com.chatting.domain.useraccount.UserAccount;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "chat_join")
+@Entity
+public class ChatJoin extends AuditingFields {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@JoinColumn(name = "chat_room_id", nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private ChatRoom chatRoom;
+
+	@JoinColumn(name = "user_account_id", nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private UserAccount userAccount;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof ChatJoin that))
+			return false;
+		return this.getId() != null && this.getId().equals(that.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.getId());
+	}
+}
---
+++ chatting/src/main/java/com/chatting/domain/chatjoin/ChatJoinRepository.java
@@ -0,0 +1,6 @@
+package com.chatting.domain.chatjoin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatJoinRepository extends JpaRepository<ChatJoin, Long> {
+}
