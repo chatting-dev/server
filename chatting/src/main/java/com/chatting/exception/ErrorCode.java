eada582a62e6548c588bbb2ef0f6371cacd1dfc7
Jeongyong Park <1239824@naver.com>
2023-03-05 18:46:18 +0900

    [feat] 유효하지 않은 토큰에 대한 예외처리

--- chatting/src/main/java/com/chatting/exception/ErrorCode.java
+++ chatting/src/main/java/com/chatting/exception/ErrorCode.java
@@ -6,7 +6,12 @@ import lombok.Getter;
 public enum ErrorCode {
 
 	// USER_ACCOUNT
-	UA_SIGNUP("회원가입 중 에러 발생");
+	UA_SIGNUP("회원가입 중 에러 발생"),
+	UA_LOGIN("로그인 중 에러발생"),
+
+	// JWT
+	JWT_INVALID("유효하지 않은 토큰"),
+	JWT_EXPIRED("유효기간이 만료된 토큰");
 
 	private final String description;
 
