9d16e2b77aa65e6926617035d3566be3b526f788
Jeongyong Park <1239824@naver.com>
2023-03-11 15:27:17 +0900

    init : 이미지 저장소 서버를 위한 모듈 생성

---
+++ imagestorage/src/test/java/com/chatting/imagestorage/ImagestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatting.imagestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagestorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
