b0bbe145e1ef6fc890e72dfa16bc466abe45cbd6
Jeongyong Park <1239824@naver.com>
2023-03-19 23:03:57 +0900

    [feat] 이미지 다운로드 기능 개발

--- imagestorage/src/main/java/com/chatting/imagestorage/application/ImageService.java
+++ imagestorage/src/main/java/com/chatting/imagestorage/application/ImageService.java
@@ -1,5 +1,7 @@
 package com.chatting.imagestorage.application;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,11 +10,13 @@ import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.chatting.imagestorage.domain.ImageFile;
 import com.chatting.imagestorage.exception.BusinessException;
 import com.chatting.imagestorage.exception.ErrorCode;
+import com.chatting.imagestorage.exception.ImageFileNotFoundException;
 import com.chatting.imagestorage.presentation.dto.ImageUploadResponse;
 
 @Service
@@ -40,6 +44,18 @@ public class ImageService {
 			return ImageUploadResponse.of(serverPrefix.concat(imageFileName));
 		} catch (final IOException e) {
 			throw new BusinessException("이미지 저장에 실패했습니다.", ErrorCode.F_INVALID);
+		}
+	}
+
+	public byte[] downloadImage(final String imageUrl) {
+		Path imageStoredPath = storagePath.resolve(imageUrl);
+
+		try (FileInputStream fis = new FileInputStream(imageStoredPath.toFile())) {
+			return StreamUtils.copyToByteArray(fis);
+		} catch (final FileNotFoundException e) {
+			throw new ImageFileNotFoundException("해당 파일이 경로에 존재하지 않습니다.", ErrorCode.F_NOT_FOUND);
+		} catch (final IOException e) {
+			throw new BusinessException("이미지 파일 변환에 실패했습니다.", ErrorCode.F_INVALID);
 		}
 	}
 }
--- imagestorage/src/main/java/com/chatting/imagestorage/presentation/ImageController.java
+++ imagestorage/src/main/java/com/chatting/imagestorage/presentation/ImageController.java
@@ -1,12 +1,16 @@
 package com.chatting.imagestorage.presentation;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.chatting.imagestorage.application.ImageService;
+import com.chatting.imagestorage.domain.FileExtension;
 import com.chatting.imagestorage.presentation.dto.ImageUploadResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -22,5 +26,12 @@ public class ImageController {
 		ImageUploadResponse response = imageService.uploadImage(image);
 
 		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/api/images/{imageUrl}")
+	public ResponseEntity<byte[]> downloadImage(@PathVariable final String imageUrl) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.contentType(FileExtension.from(imageUrl).mediaType())
+			.body(imageService.downloadImage(imageUrl));
 	}
 }
