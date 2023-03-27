5089af1f7c3a41ca2584ddad75958698a790eb7b
Jeongyong Park <1239824@naver.com>
2023-03-27 19:41:47 +0900

    [test] ETag 테스트코드 작성

--- imagestorage/src/test/java/com/chatting/imagestorage/api/ImageUploadApiTest.java
+++ imagestorage/src/test/java/com/chatting/imagestorage/api/ImageUploadApiTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -66,6 +67,36 @@ public class ImageUploadApiTest {
 			softAssertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
 			softAssertions.assertThat(res.getHeaders().getContentType()).isEqualTo(MediaType.IMAGE_JPEG);
 		});
+	}
+
+	@DisplayName("한 번 응답을 받은 후 동일한 요청을 etag 헤더와 같이 보내면 304응답코드가 반환된다.")
+	@Test
+	void givenResponseWasRetrieved_whenRetrievingAgainWithEtag_thenNotModified() {
+		// given
+		ResponseEntity<byte[]> res = testRestTemplate.getForEntity(
+			"http://localhost:8080/api/resize/images/1cad34b7-55bc-49c3-a5a8-d384c3d30c26.jpeg?width=500",
+			byte[].class
+		);
+		SoftAssertions.assertSoftly(softAssertions -> {
+			softAssertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+			softAssertions.assertThat(res.getHeaders().getCacheControl()).isNotNull();
+			softAssertions.assertThat(res.getHeaders().getETag()).isNotNull();
+		});
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setIfNoneMatch(res.getHeaders().getETag());
+		HttpEntity<Object> request = new HttpEntity<>(headers);
+
+		// when
+		ResponseEntity<byte[]> secondRes = testRestTemplate.exchange(
+			"http://localhost:8080/api/resize/images/1cad34b7-55bc-49c3-a5a8-d384c3d30c26.jpeg?width=500",
+			HttpMethod.GET,
+			request,
+			byte[].class
+		);
+
+		// then
+		assertThat(secondRes.getStatusCode()).isEqualTo(HttpStatus.NOT_MODIFIED);
 	}
 
 	@DisplayName("이미지를 다운로드할 때 이미지가 존재하지 않아 NotFound 코드가 반환된다.")
